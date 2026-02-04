0166fb1d10b6c593dff8d6bcd7539f9168dab082
asfanish23 <ADHezri@gmail.com>
2026-02-05 01:26:07 +0800

    add markdown v3

--- app/Helpers/MarkdownHelper.php
+++ app/Helpers/MarkdownHelper.php
@@ -6,6 +6,9 @@ class MarkdownHelper
 {
     public static function parse($text)
     {
+        if (!class_exists('Parsedown')) {
+            require_once base_path('vendor/parsedown/parsedown/Parsedown.php');
+        }
         return (new \Parsedown())->text($text);
     }
 }
